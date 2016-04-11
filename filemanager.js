1dc4d54de8c9509a5b29598e472f249d1f3d64a8
Bob van der Valk <info@mawoo.nl>
2016-04-11 23:43:53 +0200

    Read folders

--- filemanager.js
+++ filemanager.js
@@ -1,16 +1,16 @@
+/**
+* FileManager Example script
+* Get the content of a folder or read statictics on certain folders
+*/
 use("filemanager.js");
 print("start");
 
-
 var time1 = new Date().getTime();
-
 var test = FileManager.scanFiles(FileManager.scanners.METADATA, "F:/test3", 100000, 10);
-//var test = FileManager.getFileManagerHandler().getNestedFiles("F:/Feed The Beast/");
-
 var time2 = new Date().getTime();
+
 print("time2: "+(time2-time1));
 print("Size2 is: " + test.size());
-
 
 set = test.keySet();
 iterator = set.iterator();
