cb9604d4adb14d4b96f201bd22d5aaa9de0e45ed
Bob van der Valk <info@mawoo.nl>
2016-04-12 00:03:19 +0200

    Connect to MongoDB

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,15 @@
+/**
+* Connect to a MongoDB
+* Read files in collections
+*
+* @author Bob van der Valk
+*/
 use("mongodb.js");
 
 print("start");
 
 var mongoConnector = MongoConnector("local");
 
-var input = "{name: \"Joshua Jenster\", company: \"Mawoo\"}";
+var input = "{name: \"John Doe\", company: \"Mawoo\"}";
 
 mongoConnector.getCollection("Test_collection").insertOne(input);
