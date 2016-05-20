e4b016e9ebe7f09a1637cdfee4cae8528bbf1ed6
Bob van der Valk <info@mawoo.nl>
2016-05-20 18:26:01 +0200

    Update create-excelfile.js

--- create-excelfile.js
+++ create-excelfile.js
@@ -3,22 +3,33 @@
 *
 * @author Bob van der Valk
 */
-use("Crawler");
-use("ExcelExport");
+use("Crawler"); // Use the crawl library
+use("ExcelExport"); // Use the ExcelExport library
 
+// Crawl the website http://mawoo.nl
 var doc = Crawler.crawl("http://mawoo.nl");
+// Select a class in the HTML content
 var element = doc.select(".service-box-1 p");
 
+// Create a new excel workbook
 var sheet = ExcelExport.create("mawoo-content");
 var rowCount = 0;
 
+// Loop through the elements from the crawler
 element.forEach(function(element) {
+    // Save element in the excel sheet at rowCount and collumn zero
     sheet.addRow(rowCount).createCell(element.text(), 0);
-    debug(element.text()) ;
+    
+    // Show the text in the debugger
+    debug(element.text());
+    
+    // add one to rowCount to save the next element on a new row
     rowCount++;
     
-    sheet.save("mawoo-data.xlsx");
+    // Save the file
+    sheet.save("C:\\Users\\bobva\\Desktop\\mawoo-data.xlsx");
 });
 
+// Close the sheet
 sheet.close();
 
