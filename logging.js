47401c1744b4232de82ba4a89252179bebeee4c5
Bob van der Valk <info@mawoo.nl>
2016-06-18 20:13:43 +0200

    Logging in WCMScript

---
+++ logging.js
@@ -0,0 +1,17 @@
+/**
+* This example is to show what log messages you can use
+*
+* @author Bob van der Valk
+*/
+
+// info message
+info("This is a info message");
+
+//debug message
+debug("This is a debug message");
+
+// warning message
+warn("This is a warning message");
+
+// error message
+error("This is a error message");
